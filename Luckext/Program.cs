090bbac2c77aa3798d2925865e720996bd13ceb7
Suren Arakelyan <suren.arakelyan@elev.ga.ntig.se>
2022-08-30 10:27:00 +0200

    Initial commit

---
+++ Luckext/Program.cs
@@ -0,0 +1,21 @@
+﻿string name;
+String howoldareyou;
+
+name = ""; 
+
+Console.WriteLine("Vad heter du?");
+name = Console.ReadLine(); 
+
+Console.WriteLine($"Hej {name}");
+// Console.WriteLine(name);
+
+Console.WriteLine("Hur gammal är du");
+howoldareyou = Console.ReadLine(); 
+
+Console.WriteLine($"Hej {name}, Okej {name} du är {howoldareyou} år "); 
+
+
+
+
+Console.WriteLine("Tryck Enter för att avsluta");
+Console.ReadLine();
